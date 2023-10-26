dbcfad09ca7b196cc88fe4a4949f303c6b5f5ebd
Kandeeparaja Vithooran <vithoo24@gmail.com>
2023-10-26 10:41:12 +0530

    added

---
+++ Day 1 exam.js
@@ -0,0 +1,94 @@
+// 01.
+let a1 = [];
+console.log(a1);
+
+// 02.
+function introduceYou(){
+ return a + z + personname
+}
+
+var a = "'hello, ";
+var z = "my name is "
+var personname = "Vithooran";
+let result = introduceYou(a + z + personname)
+console.log(result)
+
+
+// 03. 
+function greetStudent(){
+    return a + studentname + c
+}
+
+var a = "'hello,+ ";
+var studentname = "Vithooran+ ";
+var c = "Welcome to the Coding School'";
+ let result1 = greetStudent(a+studentname+c);
+
+console.log(result1);
+
+// 04. 
+let person = {
+  Name2: 'sajeevan',
+  age:24,
+}
+// dot notion
+console.log(person.Name2);
+// bracket notion
+console.log(person["age"]);
+
+
+
+// 05.
+let email = ["vithoo@gmailcom"];
+ email[0] = "john@gmail.com";
+ console.log(email);
+
+
+
+// 06.
+var Name = ["vithoo", "sajeee","pirann","nilaxaa"];
+  
+  Name.pop()
+  
+console.log(Name);
+
+// 07.write a javascript program to check whether a property exixst in an object
+
+// 08.
+let student = ['Vithooran '];
+let course = ['Full stack Dev']
+
+console.log(student + course);
+
+// 09. how do you check an object has a specific property say address?
+// if there an address, log (address is there) in console, if not (address not found)
+
+function answer(address){
+  let lowerCaseQuestion = address.toLowerCase();
+
+  // if (lowerCaseQuestion == 'yes'){
+  //   return "if there is "
+  // }
+   
+}
+
+
+// 10. create an object book with peoperities for tiitle, author,and year and then convert it into a JSON string
+
+
+// 11. 
+let students = {
+    Name3: "Vithooran",
+    Age:24,
+    gpa:78,
+}
+console.log(students);
+
+
+
+
+
+
+
+
+ 
